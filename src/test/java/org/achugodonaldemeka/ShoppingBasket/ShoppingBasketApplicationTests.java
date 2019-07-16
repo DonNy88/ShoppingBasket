0bb3e5e3e2e5173e10eacb471e40d24ab3d781e1
Donald Emeka Achugo <achugo.donald@gmail.com>
2019-07-16 07:52:21 +0200

    added second test case

--- src/test/java/org/achugodonaldemeka/ShoppingBasket/ShoppingBasketApplicationTests.java
+++ src/test/java/org/achugodonaldemeka/ShoppingBasket/ShoppingBasketApplicationTests.java
@@ -1,13 +1,10 @@
 package org.achugodonaldemeka.ShoppingBasket;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,13 +12,21 @@ public class ShoppingBasketApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		Assert.isTrue(true, "The application is started well!");
+		Assert.assertTrue("The application is started well!", true);
 	}
 
 	@Test
-	public void noInput() {
+	public void noInputShouldPrint_No_input_provided() {
 		String[] argsArray = new String[]{};
 		ShoppingBasketApplication.main(argsArray);
+		Assert.assertEquals(ShoppingBasketApplication.output, "No input provided");
+	}
+
+	@Test
+	public void inputNotValidShouldPrint_This_product_x_is_not_available() {
+		String[] argsArray = new String[]{"Soup", "Milk", "pizza", "bread", "Fagioli"};
+		ShoppingBasketApplication.main(argsArray);
+		Assert.assertEquals("The product pizza is not available", ShoppingBasketApplication.output);
 	}
 
 }
