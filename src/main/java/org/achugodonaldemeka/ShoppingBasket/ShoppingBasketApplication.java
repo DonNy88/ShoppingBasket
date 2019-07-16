6849af8bdaf46be52d3a73a3484f3a3a2c1b0fb0
Donald Emeka Achugo <achugo.donald@gmail.com>
2019-07-16 07:52:21 +0200

    added second test case

--- src/main/java/org/achugodonaldemeka/ShoppingBasket/ShoppingBasketApplication.java
+++ src/main/java/org/achugodonaldemeka/ShoppingBasket/ShoppingBasketApplication.java
@@ -5,19 +5,37 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @Log4j2
 @SpringBootApplication
 public class ShoppingBasketApplication implements CommandLineRunner {
+
+    public static String output;
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingBasketApplication.class, args);
     }
 
+    static final String[] products = new String[]{"Soup", "Bread", "Milk", "Apples"};
+
     @Override
     public void run(String... args) throws Exception {
-        log.info("main started");
-        for (int i = 0; i < args.length; ++i) {
-            log.info(i + " " + args[i]);
+        if (args.length == 0) {
+            output = "No input provided";
+        } else {
+            for (int i = 0; i < args.length; ++i) {
+                String elem = args[i];
+                if (!Arrays.stream(products).anyMatch(str -> str.trim().equalsIgnoreCase(elem.trim()))) {
+                    output = "The product " + elem + " is not available";
+                    System.out.println(output);
+                    return;
+                }
+            }
         }
+
+        System.out.println(output);
     }
+
+
 }
